4ee76eeea1a2df07562c7e4bc824720e95d3ea38
daniel <danielcooder@gmail.com>
2024-02-28 21:52:02 -0300

    style: criação de de pasta js

---
+++ js/index.js
@@ -0,0 +1,36 @@
+const routes = {
+    "/": "/pages/home.html",
+    "/about": "/pages/about.html",
+    "/contact": "/pages/contact.html",
+    404: "/pages/404.html"
+}
+
+function route(event) {
+
+    event = event || window.event
+    event.preventDefault()
+
+    window.history.pushState({}, "", event.target.href)
+
+    handle()
+}
+
+function handle() {
+
+    const { pathname } = window.location
+    const route = routes[pathname] || routes[404]
+
+    console.log('antes do fecht')
+
+    fetch(route)
+        .then(data => data.text())
+        .then(html => {
+            document.querySelector('#app').innerHTML = html
+        })
+
+}
+
+handle()
+
+window.onpopstate = () => handle()
+window.route = () => route()
