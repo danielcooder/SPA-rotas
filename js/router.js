779861b8a6cf90f03c80bcc92ba816d73f47f320
daniel <danielcooder@gmail.com>
2024-03-01 16:12:51 -0300

    style: objetos

---
+++ js/router.js
@@ -0,0 +1,36 @@
+
+
+class Router {
+
+    add(routeName, page) {
+
+        this.routes[routeName] = page
+    }
+
+    route(event) {
+
+        event = event || window.event
+        event.preventDefault()
+
+        window.history.pushState({}, "", event.target.href)
+
+        this.handle()
+    }
+
+    handle() {
+
+        const { pathname } = window.location
+        const route = this.routes[pathname] || this.routes[404]
+
+        console.log('antes do fecht')
+
+        fetch(route)
+            .then(data => data.text())
+            .then(html => {
+                document.querySelector('#app').innerHTML = html
+            })
+    }
+
+}
+
+const router = new Router()
